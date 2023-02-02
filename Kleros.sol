9dc51f4d923b2ea27a36b453fa7ea9bdee2692bb
Aakash_03 <57516901+Aakash-0003@users.noreply.github.com>
2023-02-02 23:58:03 +0530

    Uploaded the 2nd exercise's solution

---
+++ Kleros.sol
@@ -0,0 +1,73 @@
+//"SPDX-License-Identifier: None
+pragma solidity ^0.8.0;
+
+contract Owner {
+
+    address public  owner;
+    address public heir;
+    
+    //event logged whenever the owner changes
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+    // modifier to check if caller is owner
+    modifier isOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+
+    /**
+     * @dev Set contract deployer as owner
+     * @param _heir initialized  
+     */
+    constructor(address _heir) {
+        require(_heir!=msg.sender,"Owner can't be the heir");
+        owner = msg.sender; 
+        heir=_heir;
+        emit OwnerSet(address(0), owner);
+    }
+    
+    /**
+     * @dev Owner Sets heir 
+     * @param newHeir set by new owner
+     */
+    function setHeir(address newHeir) public isOwner{
+    require(newHeir!=address(0)&&newHeir !=owner,"can't be the heir");
+    heir=newHeir;
+    }
+} 
+
+contract Bank is Owner{
+    uint public balance ;
+    uint private counter=block.timestamp;
+    /**
+     * @dev Set contract deployer as owner
+     * @param _heir initialized  
+     */
+    constructor(address _heir) Owner( _heir) {
+        
+    }
+    /**
+     * @dev Deposit amount 
+     */
+    function deposit() public payable {
+        balance+=msg.value;
+    }
+    /**
+     * @dev Owner Withdraws amount 
+     * @param _amount to be withdrawn
+     */
+    function withdraw(uint _amount) public isOwner returns(bool success) {
+            if(_amount==0){
+            require(block.timestamp > counter + 30 days , "Can't change owner due to time limit");
+            owner = heir;
+            emit OwnerSet(owner, heir);
+            return success=true;
+            }else {
+            balance-=_amount;
+            ( success,)=owner.call{value:_amount}("");
+            counter= block.timestamp;
+            return success;
+        }
+    }
+
+}
